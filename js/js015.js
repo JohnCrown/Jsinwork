0eefa1b434820eae0a5078725c6659f6a3745f7f
John <splaysing@gmail.com>
2020-07-20 21:21:00 +0300

    Functions and props

--- js/js015.js
+++ js/js015.js
@@ -1,5 +1,17 @@
 'use strict';
-const  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms ;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    }
+    
+}
+start();
+
 
 const personalMovieDB = {
 
@@ -12,22 +24,24 @@ const personalMovieDB = {
 
 };
 
+function rememberMyFilms() {
+    
 
 for (let i = 0; i < 2;i++){
-    const LastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+       const LastFilm = prompt("Один из последних просмотренных фильмов?", ""),
         RatingFilm = prompt("На сколько оцените его?", ""); 
-if (LastFilm != null && LastFilm != "" && LastFilm.length < 50 &&
-    RatingFilm != null && RatingFilm != "" && RatingFilm.length < 50){
-    personalMovieDB.movies[LastFilm] = RatingFilm;
-    console.log("good!");
-}else {
+       if (LastFilm != null && LastFilm != "" && LastFilm.length < 50 &&
+           RatingFilm != null && RatingFilm != "" && RatingFilm.length < 50){
+           personalMovieDB.movies[LastFilm] = RatingFilm;
+           console.log("good!");
+       }else {
     console.log("Error");
     i--;
+       }
+    }
 }
-}
 
-
-
+rememberMyFilms();
 
 // let n = 0;
 // while (n < 2) {
@@ -55,7 +69,7 @@ if (LastFilm != null && LastFilm != "" && LastFilm.length < 50 &&
 // }
 // while (k < 2)
 
-
+function detectPersonalLevel(){
 if (personalMovieDB.count < 10){
 
 console.log("Просмотрено довольно мало фильмов");
@@ -67,8 +81,47 @@ console.log("Просмотрено довольно мало фильмов");
 
 }else {
     console.log("Произошла ошибка");
+     }
+
+}
+    
+detectPersonalLevel();
+
+// function showMyDB() {
+//     if (personalMovieDB.privat == false) {
+//         console.log(personalMovieDB);
+
+        
+//     }else {
+//         console.log("ShowMyDB Error");
+//     }
+
+    
+// }
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+        
+    }else {
+        console.log("ShowMyDB Error");
+    }
 }
 
 
+
+showMyDB ();
+function writeYourGenres() {
+    for (let z = 1;z <= 3;z++) {
+        // const favoriteFilm = prompt( `Ваш любимый жанр под номером ${z} `);
+              
+        //       personalMovieDB.genres[z - 1] = favoriteFilm;
+        personalMovieDB.genres[z -1] = prompt ( `Ваш любимый жанр под номером ${z} `);
+        
+
+
+
+
+    }
     
-console.log(personalMovieDB);
+}
+writeYourGenres();
---
+++ js/js017.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const str = "text";
+
+
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello World!"
+console.log(logg.slice(6, 11));//Поддерживает отрицательные значения
+console.log(logg.substring(6, 11));
